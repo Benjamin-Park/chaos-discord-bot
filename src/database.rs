cf2b8a35456c001e2132fd680ada0153343b5860
Fulcrum <fulcrum@fulcrumdelta.dev>
2023-04-17 21:15:41 +1200

    Remove Unimplemented Features from Main

--- src/database.rs
+++
@@ -1,9 +0,0 @@
-use rusqlite::*;
-
-pub fn connect(database_file: String) -> Result<rusqlite::Connection> {
-    let connection = Connection::open(database_file.to_string()).expect("error connecting to database");
-
-    Ok(connection)
-}
-
-
